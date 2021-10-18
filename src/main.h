86a7a64c81d46bf180a1b1c293772f4675543e07
KingricharDV#0439 <northerncommunity1@gmail.com>
2021-10-17 14:12:43 -1000

    update
    updated protocol, client version, changed payout and add a checkpoint

--- src/main.h
+++ src/main.h
@@ -52,7 +52,7 @@ inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MO
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
-static const int64_t COIN_YEAR_REWARD = 85 * CENT; //RR Zafar
+static const int64_t COIN_YEAR_REWARD = 6 * CENT; //RR Zafar
 
 static const uint256 hashGenesisBlock("0x000003e4086c6030369ce90af75ec0b93e534f1072023d92090dcc4365522bb0"); //RR Zafar
 static const uint256 hashGenesisBlockTestNet("0x000003e4086c6030369ce90af75ec0b93e534f1072023d92090dcc4365522bb0"); //RR Zafar
@@ -1111,7 +1111,7 @@ public:
     int64_t nMoneySupply;
 
     unsigned int nFlags;  // ppcoin: block index flags
-    enum  
+    enum
     {
         BLOCK_PROOF_OF_STAKE = (1 << 0), // is proof-of-stake block
         BLOCK_STAKE_ENTROPY  = (1 << 1), // entropy bit for stake modifier
@@ -1303,7 +1303,7 @@ public:
             pprev, pnext, nFile, nBlockPos, nHeight,
             FormatMoney(nMint).c_str(), FormatMoney(nMoneySupply).c_str(),
             GeneratedStakeModifier() ? "MOD" : "-", GetStakeEntropyBit(), IsProofOfStake()? "PoS" : "PoW",
-            nStakeModifier, nStakeModifierChecksum, 
+            nStakeModifier, nStakeModifierChecksum,
             hashProof.ToString().c_str(),
             prevoutStake.ToString().c_str(), nStakeTime,
             hashMerkleRoot.ToString().c_str(),
