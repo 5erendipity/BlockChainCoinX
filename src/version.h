d64f36de8e5bad419df8dc9ca17ff57c3463af17
5erendipity <76568125+5erendipity@users.noreply.github.com>
2022-03-04 15:21:34 +0000

    Protocol Update
    Update Protocol version range

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 61116;     //CD
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 61016;   //CD
+static const int MIN_PEER_PROTO_VERSION = 61116;   //CD
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -44,7 +44,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;    //CD
-static const int NOBLKS_VERSION_END = 61015;     //CD
+static const int NOBLKS_VERSION_END = 61115;     //CD
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
